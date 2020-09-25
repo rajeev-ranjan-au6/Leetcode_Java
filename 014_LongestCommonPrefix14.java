9a8d288c9e10f019075da675c326004c1128aca4
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 10:28:31 +0530

    Update 014_LongestCommonPrefix14.java

--- 014_LongestCommonPrefix14.java
+++ 014_LongestCommonPrefix14.java
@@ -1,83 +1,142 @@
-/**
- * Write a function to find the longest common prefix string amongst an array of strings.
- */
+class Solution {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        Trie trie = new Trie();
+        String searchString = null;
+        for (String word : strs) {
+            if (searchString == null) {
+                searchString = word;
+            } else if (searchString.length() > word.length()) {
+                searchString = word;
+            }
+            trie.insert(word);
+        }
+        return trie.longestCommonPrefix(searchString);
+    }
+}
 
+class Trie {
+    public static class TrieNode {
+        Map<Character, TrieNode> map;
+        boolean isWord;
 
-public class LongestCommonPrefix14 {
-    public String longestCommonPrefix(String[] strs) {
-        String lcp = "";
-        int S = strs.length;
-        if (S == 0) return lcp;
+        public TrieNode() {
+            map = new HashMap<>();
+            isWord = false;
+        }
+    }
 
-        int L = strs[0].length();
-        int idx = 0;
-        for (int i = 1; i < S; i++) {
-            if (strs[i].length() < L) {
-                L = strs[i].length();
-                idx = i;
+    private final TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            TrieNode newNode = current.map.get(word.charAt(i));
+            if (newNode == null) {
+                newNode = new TrieNode();
+                current.map.put(word.charAt(i), newNode);
+            }
+            current = newNode;
+        }
+        current.isWord = true;
+    }
+
+    public String longestCommonPrefix(String word) {
+        TrieNode current = root;
+        if (current.map.size() > 1) return "";
+        String longestCommonPrefix = "";
+        for (int i = 0; i < word.length(); i++) {
+            TrieNode newNode = current.map.get(word.charAt(i));
+            if (newNode == null || newNode.map.size() > 1) return longestCommonPrefix + word.charAt(i);
+            else {
+                longestCommonPrefix += word.charAt(i);
+                current = newNode;
             }
         }
-        if (L == 0) return lcp;
+        return longestCommonPrefix;
+    }
 
-        int p = 1;
-        String possible = strs[idx];
-        while (p <= L) {
-            String sub = possible.substring(0, p);
-            boolean b = true;
-            for (int k = 0; k < S; k++) {
-                if (!strs[k].startsWith(sub)) {
-                    b = false;
+}
+
+*********************************************************************
+
+class Solution {
+    
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+        int count = lengthOfLongestCommonPrefix(strs);
+        return strs[0].substring(0, count);
+    }
+    
+    private int lengthOfLongestCommonPrefix(String[] strs){
+        int count = 0;
+        for(int i = 0 ; i < strs[0].length(); i++){
+            boolean same = true;
+            char ch = ' ';
+            for(int j = 0; j < strs.length; j++){
+                if(i >= strs[j].length()){
+                    return count;
+                }
+                if(j == 0){
+                    ch = strs[j].charAt(i);
+                    continue;
+                }
+                if(ch != strs[j].charAt(i)){
+                    same = false;
+                }
+            }
+            
+            if(same){
+                count ++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
+    }
+}
+
+*************************************************************************
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0)
+            return "";
+        boolean flag = true;
+        int count = 0;
+        int smallestLen = strs[0].length();
+        for(int i=1;i<strs.length;i++)
+        {
+            if(strs[i].length()<smallestLen)
+                smallestLen = strs[i].length();
+        }
+        for(int i=0;i<smallestLen;i++)
+        {
+            char c = strs[0].charAt(i);
+            for(int j=1;j<strs.length;j++)
+            {
+                if(strs[j].charAt(i)==c)
+                    flag=true;
+                else
+                {
+                    flag=false;
                     break;
                 }
             }
-            if (!b) break;
-            lcp = sub;
-            p++;
+            if(flag==true)
+                count++;
+            else
+                break;
         }
-
-        return lcp;
+        if(count==0)
+            return "";
+        return strs[0].substring(0,count);
     }
-
-
-    /**
-     * https://discuss.leetcode.com/topic/6987/java-code-with-13-lines
-     */
-    public String longestCommonPrefix2(String[] strs) {
-        if(strs == null || strs.length == 0)    return "";
-        String pre = strs[0];
-        int i = 1;
-        while(i < strs.length){
-            while(strs[i].indexOf(pre) != 0)
-                pre = pre.substring(0,pre.length()-1);
-            i++;
-        }
-        return pre;
-    }
-
-
-    /**
-     * https://discuss.leetcode.com/topic/27913/sorted-the-array-java-solution-2-ms
-     */
-    public String longestCommonPrefix3(String[] strs) {
-        StringBuilder result = new StringBuilder();
-
-        if (strs!= null && strs.length > 0){
-
-            Arrays.sort(strs);
-
-            char [] a = strs[0].toCharArray();
-            char [] b = strs[strs.length-1].toCharArray();
-
-            for (int i = 0; i < a.length; i ++){
-                if (b.length > i && b[i] == a[i]){
-                    result.append(b[i]);
-                }
-                else {
-                    return result.toString();
-                }
-            }
-        }
-        return result.toString();
-    }
-
 }
