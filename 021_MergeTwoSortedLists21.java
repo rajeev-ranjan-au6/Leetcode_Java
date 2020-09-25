01211de804cded38f12a899b4e762e7b07eee075
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 11:48:03 +0530

    Update 021_MergeTwoSortedLists21.java

--- 021_MergeTwoSortedLists21.java
+++ 021_MergeTwoSortedLists21.java
@@ -1,54 +1,36 @@
-/**
- * Merge two sorted linked lists and return it as a new list. The new list
- * should be made by splicing together the nodes of the first two lists.
- *
- * Example:
- *
- * Input: 1->2->4, 1->3->4
- * Output: 1->1->2->3->4->4
- *
- */
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-
-public class MergeTwoSortedLists21 {
+class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode p = dummy;
-        while (l1 != null && l2 != null) {
-            if (l1.val >= l2.val) {
-                p.next = l2;
-                l2 = l2.next;
-            } else {
-                p.next = l1;
-                l1 = l1.next;
-            }
-            p = p.next;
-            p.next = null;
+        ListNode r = l1, l = l2;
+        ListNode dummyNode = new ListNode(0);
+        ListNode curr = dummyNode;
+        while(r != null || l != null){
+            int rValue = (r == null) ? Integer.MAX_VALUE : r.val;
+            int lValue = (l == null) ? Integer.MAX_VALUE : l.val;
+            int minValue = Math.min(rValue, lValue);
+            if(rValue == minValue){r = r.next;} 
+            else {l = l.next;}
+            curr.next = new ListNode(minValue);
+            curr = curr.next;
         }
-        if (l1 != null) p.next = l1;
-        if (l2 != null) p.next = l2;
-        return dummy.next;
+        return dummyNode.next;
     }
-
-    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-
-        if (l1.val >= l2.val) {
-            l2.next = mergeTwoLists2(l1, l2.next);
-            return l2;
-        } else {
-            l1.next = mergeTwoLists2(l1.next, l2);
-            return l1;
-        }
-    }
-
 }
+
+*********************************************************************
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0);
+        ListNode tail = res, p1 = l1, p2 = l2;
+        while (p1 != null && p2 != null) {
+            tail.next = new ListNode(p1.val < p2.val ? p1.val : p2.val);
+            tail = tail.next;
+            if (p1.val < p2.val) p1 = p1.next;
+            else p2 = p2.next;
+        }
+        tail.next = p1 == null ? p2 : p1;
+        return res.next;
+    }
+}
+
+***************************************************
