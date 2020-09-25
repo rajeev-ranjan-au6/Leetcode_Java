a22bbfbeba7ae42e9cad160c8904f5473b71935f
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 12:06:34 +0530

    Update 025_ReverseNodesInKGroup25.java

--- 025_ReverseNodesInKGroup25.java
+++ 025_ReverseNodesInKGroup25.java
@@ -1,175 +1,150 @@
-/**
- * Given a linked list, reverse the nodes of a linked list k at a time and
- * return its modified list.
- *
- * k is a positive integer and is less than or equal to the length of the linked
- * list. If the number of nodes is not a multiple of k then left-out nodes in
- * the end should remain as it is.
- *
- * You may not alter the values in the nodes, only nodes itself may be changed.
- *
- * Only constant memory is allowed.
- *
- * For example,
- * Given this linked list: 1->2->3->4->5
- *
- * For k = 2, you should return: 2->1->4->3->5
- *
- * For k = 3, you should return: 3->2->1->4->5
- */
+class Solution {
 
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-
-
-public class ReverseNodesInKGroup25 {
     public ListNode reverseKGroup(ListNode head, int k) {
-
-        if (k == 0 || k == 1) {
-            return head;
-        }
-
-        if (head == null) {
-            return null;
-        }
-
-        ListNode now = head;
-        int i = 1;
-        ListNode r = new ListNode(0);
-        ListNode last = r;
-        ListNode tempHead = null;
-        ListNode tempTail = null;
-        while (now != null) {
-            ListNode temp = new ListNode(now.val);
-            if (i == 1) {
-                tempHead = temp;
-                tempHead.next = null;
-                tempTail = temp;
-                tempTail.next = null;
-                i++;
-            } else if (i == k) {
-                temp.next = tempHead;
-                tempHead = temp;
-                last.next = tempHead;
-                last = tempTail;
-                tempHead = null;
-                tempTail = null;
-                i = 1;
-            } else {
-                temp.next = tempHead;
-                tempHead = temp;
-                i++;
+        if (k <= 1) return head;
+        int len = 1;
+        ListNode firstTail = head;
+        
+        //Loop to find the tail of the group starting by head
+        //The group must be k-length
+        for(; firstTail != null && len < k; len++) firstTail = firstTail.next;
+        
+        if (firstTail == null || len < k) return head; //k is greater than num of total nodes, or the last nodes don't make a k-length group (a non-group)
+        
+        ListNode tail, auxHead, aux1, aux2, aux3;
+        while (len == k) {
+            auxHead = head;
+            aux1 = head;
+            aux2 = aux1.next;
+            aux3 = null;
+            
+            //Loop to reverse a group
+            //It takes k-1 iterations (for the group {1 -> 2}, we just have to make {2 -> 1}, which takes one iteration)
+            //This loop is to reverse a group, we still have to join 2 different groups, which takes place outside this loop
+            for(int i = 0; i < k-1; i++) {
+                aux3 = aux2.next;
+                aux2.next = aux1;
+                aux1 = aux2;
+                aux2 = aux3;
             }
-            now = now.next;
-        }
-
-        ListNode tailHead = null;
-        while (tempHead != null) {
-            ListNode temp = new ListNode(tempHead.val);
-            temp.next = tailHead;
-            tailHead = temp;
-            last.next = tailHead;
-            tempHead = tempHead.next;
-        }
-
-        return r.next;
-    }
-
-
-    /**
-     * https://discuss.leetcode.com/topic/7126/short-but-recursive-java-code-with-comments/27
-     */
-    public ListNode reverseKGroup2(ListNode head, int k) {
-        ListNode curr = head;
-        int count = 0;
-        while (curr != null && count != k) { // find the k+1 node
-            curr = curr.next;
-            count++;
-        }
-        if (count == k) { // if k+1 node is found
-            curr = reverseKGroup(curr, k); // reverse list with k+1 node as head
-            // head - head-pointer to direct part,
-            // curr - head-pointer to reversed part;
-            while (count-- > 0) { // reverse current k-group:
-                ListNode tmp = head.next; // tmp - next head in direct part
-                head.next = curr; // preappending "direct" head to the reversed list
-                curr = head; // move head of reversed part to a new node
-                head = tmp; // move "direct" head to the next node in direct part
+            
+            //Since aux3 is the next node which the head of the actual group had before it was reversed, we find the tail of this new group starting by aux3
+            //The code is the same as above
+            len = 1;
+            head = aux3;
+            tail = head;
+            for(; tail != null && len < k; len++) tail = tail.next;
+            
+            //There wasn't enough nodes to make a k-length group (a non-group), so we decrease its length so it doesn't count the null node
+            //Also, the tail of the previous calculated group will point to the head of the non-group
+            if (tail == null) {
+                tail = head;
+                len--;
             }
-            head = curr;
+            auxHead.next = tail;
         }
-        return head;
+        return firstTail;
     }
+}
 
+***************************************************************************************************
 
-    /**
-     * https://discuss.leetcode.com/topic/7126/short-but-recursive-java-code-with-comments/27
-     */
-    public ListNode reverseKGroup3(ListNode head, int k) {
-        int n = 0;
-        for (ListNode i = head; i != null; n++, i = i.next);
-
-        ListNode dmy = new ListNode(0);
-        dmy.next = head;
-        for(ListNode prev = dmy, tail = head; n >= k; n -= k) {
-            for (int i = 1; i < k; i++) {
-                ListNode next = tail.next.next;
-                tail.next.next = prev.next;
-                prev.next = tail.next;
-                tail.next = next;
-            }
-
-            prev = tail;
-            tail = tail.next;
-        }
-        return dmy.next;
-    }
-
-    /**
-     * https://leetcode.com/problems/reverse-nodes-in-k-group/#/solutions
-     */
-    public ListNode reverseKGroup4(ListNode head, int k) {
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
         ListNode begin;
-        if (head==null || head.next ==null || k==1) return head;
+        if (head==null || head.next ==null || k==1)
+            return head;
         ListNode dummyhead = new ListNode(-1);
         dummyhead.next = head;
         begin = dummyhead;
         int i=0;
         while (head != null){
-          	i++;
-          	if (i%k == 0){
-            		begin = reverse(begin, head.next);
-            		head = begin.next;
-          	} else {
-          		  head = head.next;
-          	}
+            i++;
+            if (i%k == 0){
+                begin = reverse(begin, head.next);
+                head = begin.next;
+            } else {
+                head = head.next;
+            }
         }
         return dummyhead.next;
-
     }
 
     public ListNode reverse(ListNode begin, ListNode end){
-      	ListNode curr = begin.next;
-      	ListNode next, first;
-      	ListNode prev = begin;
-      	first = curr;
-      	while (curr!=end){
-        		next = curr.next;
-        		curr.next = prev;
-        		prev = curr;
-        		curr = next;
-      	}
-      	begin.next = prev;
-      	first.next = curr;
-      	return first;
+        ListNode curr = begin.next;
+        ListNode next, first;
+        ListNode prev = begin;
+        first = curr;
+        while (curr!=end){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        begin.next = prev;
+        first.next = curr;
+        return first;
     }
+}
 
-
+*************************************************************************************************************************
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode newHead = null;
+        ListNode newPtr = null;
+        ListNode current = head;
+        ListNode first = null;
+        ListNode cap = null;
+        
+        while(current != null){
+            // Gonna need this to reverse the k group
+            first = current;
+            
+            // Get the k group
+            for(int i = 0; i < k-1; i++){
+                if(current == null) break;
+                current = current.next;
+            }
+            
+            // If we have a full k group
+            if(current != null){
+                cap = current.next;
+                current.next = null;
+                ListNode reversed = reverse(first);
+                
+                // Add to the end or head of our new linked list
+                if(newHead == null){
+                    newHead = reversed;
+                    newPtr = newHead;
+                }
+                else newPtr.next = reversed;
+                while(newPtr != null && newPtr.next != null) newPtr = newPtr.next;
+            }
+            
+            // HANDLE REMAINING NODES
+            else{
+                newPtr.next = first;
+                break;
+            }
+            // Resume where we left off
+            current = cap;
+        }
+        return newHead;
+    }
+    
+    public ListNode reverse(ListNode head) {
+        if(head == null) return null;
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode temp = null;
+        
+        while(current != null){
+            temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        head.next = null;
+        return prev;
+    }
 }
