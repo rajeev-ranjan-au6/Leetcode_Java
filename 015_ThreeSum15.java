e1bf92882e81a595c908114f9cdccb7b18f47085
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 10:34:50 +0530

    Update 015_ThreeSum15.java

--- 015_ThreeSum15.java
+++ 015_ThreeSum15.java
@@ -1,68 +1,146 @@
-/**
- * Given an array S of n integers, are there elements a, b, c in S such that
- * a + b + c = 0? Find all unique triplets in the array which gives the sum
- * of zero.
- *
- * Note: The solution set must not contain duplicate triplets.
- *
- * For example, given array S = [-1, 0, 1, 2, -1, -4],
- *
- * A solution set is:
- * [
- *   [-1, 0, 1],
- *   [-1, -1, 2]
- * ]
- */
-
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.LinkedList;
-
-public class ThreeSum15 {
-
-    /**
-     * https://discuss.leetcode.com/topic/8125/concise-o-n-2-java-solution
-     */
+class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length < 3) return res;
-        Arrays.sort(nums);
-        if (nums[0] > 0 || nums[nums.length-1] < 0) return res;
+        if (nums.length < 3) return new ArrayList<>(); // if nums less than 3 element
+        Arrays.sort(nums); // sort array
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) set.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                else if (sum > 0) k--;
+                else if (sum < 0) j++;
+            }
 
-        for (int i=0; i<nums.length-2; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
+        }
 
-            int l = i+1;
-            int r = nums.length-1;
-            int left = 0 - nums[i];
-            while (l < r) {
-                if (nums[l] + nums[r] == left) {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    while (l < r && nums[l] == nums[l+1]) l++;
-                    while (l < r && nums[r] == nums[r-1]) r--;
-                    l++;
-                    r--;
-                } else if (nums[l] + nums[r] > left) {
-                    r--;
-                } else {
-                    l++;
+        return new ArrayList<>(set);
+    }
+}
+
+*************************************************************************************************
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+                Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++)
+        {
+            if(i==0 || (i>0 && nums[i]!=nums[i-1]))
+            {
+                int lo=i+1,hi=nums.length-1;
+                int target=0-nums[i];
+                while(lo<hi)
+                {
+                    int sum=nums[lo]+nums[hi];
+                    if(sum==target)
+                    {
+                        res.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
+                        while(lo<hi && nums[lo]==nums[lo+1])lo++;
+                        while(lo<hi && nums[hi]==nums[hi-1])hi--;
+                        lo++;
+                        hi--;
+                    }
+                    else if(sum>target)
+                    {
+                        hi--;
+                    }
+                    else
+                    {
+                        lo++;
+                    }
                 }
             }
         }
-
         return res;
     }
+}
 
-    public static void main(String[] args) {
-        ThreeSum15 ts = new ThreeSum15();
+***********************************************************************
 
-        System.out.println(ts.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
-        System.out.println(ts.threeSum(new int[]{0, 0, 0}));
-        System.out.println(ts.threeSum(new int[]{0, 0, 0, 0}));
-        System.out.println(ts.threeSum(new int[]{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}));
-        System.out.println(ts.threeSum(new int[]{0, 0, 0, 1, 1}));
-        System.out.println(ts.threeSum(new int[]{-1, -1, 0, 0, 0}));
+/*
+    Video explanation: https://youtu.be/1aQvO4JPo70
+*/
+class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+       List<List<Integer>> result = new ArrayList<List<Integer>>();
+    	if(nums == null || nums.length < 3)
+        	return result;
+    	// sort the array in-place
+    	Arrays.sort(nums);
+    	for (int i = 0; i < nums.length - 2; i++) {
+    	    if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+    	        int low = i+1, high = nums.length - 1;
+    	        int sum = 0 - nums[i];
+                // Finding two sum using two pointer
+    	        while (low < high) {
+    	           if (nums[low] + nums[high] == sum) {
+    	              result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                      // if next number is duplicates skip that
+    	              while (low < high && nums[low] == nums[low+1]) low++;
+                      // if previous number is duplicates skip that
+    	              while (low < high && nums[high] == nums[high-1]) high--;
+    	              low++; 
+		              high--;
+    	           } else if (nums[low] + nums[high] < sum) {
+    	              low++;
+    	           } else {
+    	              high--;
+    	           }
+    	        }
+    	    }
+    	}
+    	
+       return result;
     }
+}
 
+***************************************************************************
+
+//Two pointer Solution
+//Sort the array
+//Get an element from the array
+//Find it has pair from the remaining elements of the array
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> triplets = new ArrayList();
+        for(int i=0; i<nums.length-2; i++)
+        {
+            if(i>0 && nums[i] == nums[i-1])  //To skip the same element
+                continue;
+            searchPair(-nums[i], i+1, nums, triplets); 
+        }
+        return triplets;
+        
+    }
+    
+    public static void searchPair(int target, int left, int[] arr, List<List<Integer>> triplets)
+    {
+        int right = arr.length-1;
+        while(left < right)
+        {
+            int currentSum = arr[left] + arr [right];
+            if(currentSum == target)
+            {
+                triplets.add(Arrays.asList(-target, arr[left], arr[right]));
+                left++;
+                right--;
+                while(left<right && arr[left] == arr[left-1]) //skip the same element
+                    left++;
+                while(left<right && arr[right] == arr[right+1]) //skip the same element
+                    right--;
+            }
+            else if(target > currentSum)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+    }
 }
