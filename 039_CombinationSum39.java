1fd9b8c391e5acf321634e933822d35d0eb5af9c
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 12:55:08 +0530

    Update 039_CombinationSum39.java

--- 039_CombinationSum39.java
+++ 039_CombinationSum39.java
@@ -1,59 +1,84 @@
-/**
- * Given a set of candidate numbers (candidates) (without duplicates) and a
- * target number (target), find all unique combinations in candidates where
- * the candidate numbers sums to target.
- * 
- * The same repeated number may be chosen from candidates unlimited number of times.
- * 
- * Note:
- * All numbers (including target) will be positive integers.
- * The solution set must not contain duplicate combinations.
- * 
- * Example 1:
- * Input: candidates = [2,3,6,7], target = 7,
- * A solution set is:
- * [
- *   [7],
- *   [2,2,3]
- * ]
- * 
- * Example 2:
- * Input: candidates = [2,3,5], target = 8,
- * A solution set is:
- * [
- *   [2,2,2,2],
- *   [2,3,3],
- *   [3,5]
- * ]
- */
-
-public class CombinationSum39 {
+class Solution {
+    List<List<Integer>> comb;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        helper(candidates, target, 0, new ArrayList<Integer>(), res);
-        return res;
+        comb = new ArrayList();
+        List<Integer> lt = new ArrayList();
+        findSum(lt, candidates, 0, target);
+        return comb;
     }
     
-    private void helper(int[] candidates, int target, int start, List<Integer> path, List<List<Integer>> res) {
-        if (target < 0) return;
-        if (target == 0) {
-            List<Integer> newPath = new ArrayList<Integer>();
-            for (Integer i: path) newPath.add(i);
-            res.add(newPath);
-        }
-    
-        for (int i = start; i<candidates.length; i++) {
-            int c = candidates[i];
-            int count =  target / c;
-            if (count >= 1) {
-                for (int j=1; j<= count; j++) {path.add(c);}
-                while (count >= 1) {
-                    helper(candidates, target - c * count, i+1, path, res);
-                    path.remove(path.size() - 1);
-                    count--;
-                }
+    private void findSum(List<Integer> sumList, int[] candidates, int beg, int target){
+        if(target == 0){
+                comb.add(sumList);
+                return;
             }
+		if(beg == candidates.length){
+            return;
+        }
+        if(candidates[beg] <= target){
+            List<Integer> lst = new ArrayList();
+            lst.addAll(sumList);
+            lst.add(candidates[beg]);
+            findSum(lst, candidates, beg, target - candidates[beg]);
+        }
+        findSum(sumList, candidates, beg+1, target);
+        
+    }
+}
+
+***************************************************************************************************
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        helperFunction(result, current, candidates, 0, target, 0);
+        return result;
+    }
+    public void helperFunction(List<List<Integer>> result, List<Integer> current, int [] candidates, int start, int target, int sum) {
+        if (sum > target)
+            return;
+        if (sum == target) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i=start; i<candidates.length; i++) {
+            current.add(candidates[i]);
+            helperFunction(result, current, candidates, i, target, sum+candidates[i]);
+            current.remove(current.size()-1);
+        }
+            
+    }
+}
+
+*************************************************************************************
+
+class Solution {
+    List<List<Integer>> resultList = new ArrayList<>();
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+
+        helper(candidates, target, 0, 0, new ArrayList<>());
+
+        return resultList;
+
+    }
+
+    private void helper(int[] candidates, int target, int index, int sum, List<Integer> list) {
+
+        if (sum > target) return;
+
+        if (sum == target) {
+
+            resultList.add(new ArrayList(list));
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            
+            list.add(candidates[i]);
+            helper(candidates, target, i , sum + candidates[i], list);
+            list.remove(list.size() - 1);
+
         }
     }
-  
 }
