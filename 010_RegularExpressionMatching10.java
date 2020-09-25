087aec119d55238e7c505bd7ae4f39a36638c0f1
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 10:06:19 +0530

    Update 010_RegularExpressionMatching10.java

--- 010_RegularExpressionMatching10.java
+++ 010_RegularExpressionMatching10.java
@@ -21,132 +21,142 @@
  */
 
 
-public class RegularExpressionMatching10 {
-
-    /**
-     * https://leetcode.com/problems/regular-expression-matching/solution/
-     */
-    public boolean isMatch(String text, String pattern) {
-        if (pattern.isEmpty()) return text.isEmpty();
-        boolean first_match = (!text.isEmpty() &&
-                               (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
-
-        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
-            return (isMatch(text, pattern.substring(2)) ||
-                    (first_match && isMatch(text.substring(1), pattern)));
-        } else {
-            return first_match && isMatch(text.substring(1), pattern.substring(1));
-        }
+class Solution {
+    public boolean isMatch(String s, String p) {
+        HashMap<String, HashMap<String, Boolean>> cache = new HashMap<>();
+        return isMatchHelper(s,p,cache);
+        
     }
-
-
-    public boolean isMatch2(String text, String pattern) {
-        return isMatch(0, 0, text, pattern);
-    }
-
-    public boolean isMatch(int i, int j, String text, String pattern) {
-        if (j == pattern.length()) return i == text.length();
-        boolean first_match = (i != text.length() &&
-                               (pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.'));
-
-        if (pattern.length() >= j+2 && pattern.charAt(j+1) == '*'){
-            return (isMatch(i, j+2, text, pattern) ||
-                    (first_match && isMatch(i+1, j, text, pattern)));
-        } else {
-            return first_match && isMatch(i+1, j+1, text, pattern);
-        }
-    }
-
-
-    public boolean isMatch3(String text, String pattern) {
-        int textLen = text.length();
-        int pattLen = pattern.length();
-        boolean[][] dp = new boolean[textLen+1][pattLen+1];
-        dp[0][0] = true;
-
-        for(int j=1; j<=pattLen; j++) {
-            if (pattern.charAt(j-1) == '*'){
-                dp[0][j] = dp[0][j-2];
+    
+    public boolean isMatchHelper(String s, String p, HashMap<String, HashMap<String, Boolean>> cache){
+        if(cache.containsKey(s)){
+            if(cache.get(s).containsKey(p)){
+                return cache.get(s).get(p);
             }
         }
-
-        for (int i=1; i<=textLen; i++) {
-            for(int j=1; j<=pattLen; j++) {
-                if (j >= 2 && pattern.charAt(j-1) == '*') {
-                    boolean preMatch = (pattern.charAt(j-2) == text.charAt(i-1) || pattern.charAt(j-2) == '.');
-                    dp[i][j] = dp[i][j-2] || (preMatch && dp[i-1][j]);
-                } else {
-                    boolean currMatch = (pattern.charAt(j-1) == text.charAt(i-1) || pattern.charAt(j-1) == '.');
-                    dp[i][j] = currMatch && dp[i-1][j-1];
+        if(s.length()==0 && p.length()==0) return true;
+        if(s.length()==0 || p.length()==0){
+            if(p.length()==0 || (p.length() != 0 && !p.contains("*"))) return false;
+            
+        }
+        
+        boolean foundMatch = false;
+        if(s.length()==0){
+            if(p.length()>1 && p.charAt(1)=='*') foundMatch = foundMatch || isMatchHelper(s,p.substring(2), cache);
+        }
+        else{
+                if(s.charAt(0)==p.charAt(0) || p.charAt(0)=='.'){
+                    foundMatch = foundMatch || isMatchHelper(s.substring(1), p.substring(1), cache);
                 }
-            }
+
+                if(p.length() > 1 && p.charAt(1)=='*'){
+                    if(s.charAt(0)==p.charAt(0) || p.charAt(0)=='.'){
+                        if(p.length()>1) {
+                            foundMatch = foundMatch || isMatchHelper(s.substring(1), p.substring(2), cache);
+                            foundMatch = foundMatch || isMatchHelper(s, p.substring(2), cache);
+                        }
+                        foundMatch = foundMatch || isMatchHelper(s.substring(1), p.substring(1), cache);
+                        foundMatch = foundMatch || isMatchHelper(s.substring(1), p, cache);                 
+
+                    }else{
+                        if(p.length()>1) foundMatch = foundMatch || isMatchHelper(s, p.substring(2), cache);
+                    }
+                }            
         }
-
-        return dp[text.length()][pattern.length()];
-    }
-
-
-    /**
-     * https://leetcode.com/problems/regular-expression-matching/solution/
-     */
-    enum Result {
-        TRUE, FALSE
-    }
-    Result[][] memo;
-
-    public boolean isMatch4(String text, String pattern) {
-        memo = new Result[text.length() + 1][pattern.length() + 1];
-        return dp(0, 0, text, pattern);
-    }
-
-    public boolean dp(int i, int j, String text, String pattern) {
-        if (memo[i][j] != null) {
-            return memo[i][j] == Result.TRUE;
+        
+        if(cache.containsKey(s)){
+            HashMap<String, Boolean> incache = cache.get(s);
+            incache.put(p,foundMatch);
+        }else{
+            HashMap<String, Boolean> incache = new HashMap<>();
+            incache.put(p,foundMatch);
+            cache.put(s,incache);
         }
+        return foundMatch;
+    }
+}
+
+***************************************************************************************************
+
+class Solution {
+    int n, m;
+    int[][] memo;
+    public boolean isMatch(String s, String p) {
+        if(s.equals(p)) return true;
+        n = s.length();
+        m = p.length();
+        memo = new int[n + 1][m + 1];
+        return match(0, 0, s, p);
+    }
+    
+    public boolean match(int i, int j, String s, String p) {
+        if(j == m) return i == n;
+        if(i < n && j < m && memo[i][j] != 0) return memo[i][j] == 1;
+
         boolean ans;
-        if (j == pattern.length()){
-            ans = i == text.length();
-        } else{
-            boolean first_match = (i < text.length() &&
-                                   (pattern.charAt(j) == text.charAt(i) ||
-                                    pattern.charAt(j) == '.'));
-
-            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                ans = (dp(i, j+2, text, pattern) ||
-                       first_match && dp(i+1, j, text, pattern));
-            } else {
-                ans = first_match && dp(i+1, j+1, text, pattern);
-            }
-        }
-        memo[i][j] = ans ? Result.TRUE : Result.FALSE;
+        boolean firstMatch = (i < n && s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+        if(j < m - 1 && p.charAt(j + 1) == '*') ans =  match(i, j + 2, s, p) || (firstMatch && i < n && match(i + 1, j, s, p));
+        else ans =  firstMatch && match(i + 1, j + 1, s, p);
+        if(i < n && j < m)
+            memo[i][j] = ans?1:-1;
         return ans;
     }
+}
 
+******************************************************************************************
 
-    public boolean isMatch5(String s, String p) {
-        if (s == null || p == null) return false;
-
-        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
-        dp[0][0] = true;
-        for (int j=1; j<=p.length(); j++) dp[0][j] = p.charAt(j-1) == '*' ? dp[0][j-2] : false;
-        for (int i=1; i<=s.length(); i++) dp[i][0] = false;
-
-        for (int i=1; i<=s.length(); i++) {
-            for (int j=1; j<=p.length(); j++) {
-                char c = p.charAt(j-1);
-                switch (c) {
-                    case '.':
-                        dp[i][j] = dp[i-1][j-1];
-                        break;
-                    case '*':
-                        dp[i][j] = dp[i][j-2] || ((p.charAt(j-2) == '.' || p.charAt(j-2) == s.charAt(i-1)) && dp[i-1][j]);
-                        break;
-                    default:
-                        dp[i][j] = s.charAt(i-1) == p.charAt(j-1) && dp[i-1][j-1];
-                }
+class Solution {
+    public boolean isMatch(String s, String p) {
+        
+        if(p.length()==0) return s.length()==0; // base case are we finished with the letters?
+        
+        if(p.length()>1 && p.charAt(1)=='*'){
+            if(isMatch(s,p.substring(2))){ //0 repeat
+                return true; 
             }
+             //1 or more than 1 repeat
+            if(s.length()>0 && (p.charAt(0)=='.' || s.charAt(0)==p.charAt(0)))
+                return isMatch(s.substring(1),p);
         }
-        return dp[s.length()][p.length()];
+        else{
+            if(s.length()>0 && (p.charAt(0)=='.' || s.charAt(0)==p.charAt(0)))
+                return isMatch(s.substring(1),p.substring(1));
+        }
+        
+        return false;
     }
+}
 
+****************************************************************
+
+class Solution {
+    Map<String,Boolean> map = new HashMap<>();
+    
+    public boolean isMatch(String s, String p) {
+        
+        if(p.length()==0) return s.length()==0; // base case are we finished with the letters in both the strings?
+        
+        String key = s+"#"+p;
+        
+        if(map.containsKey(key)) return map.get(key);
+        
+        Boolean result=false;
+        
+        if(p.length()>1 && p.charAt(1)=='*'){
+            if(isMatch(s,p.substring(2))){ //0 repeat
+				result = true;
+                map.put(key,result);
+                return result;
+            }
+             //1 or more than 1 repeat
+            if(s.length()>0 && (p.charAt(0)=='.' || s.charAt(0)==p.charAt(0)))
+                result = isMatch(s.substring(1),p);
+        }
+        else{
+            if(s.length()>0 && (p.charAt(0)=='.' || s.charAt(0)==p.charAt(0)))
+                result = isMatch(s.substring(1),p.substring(1)); //both match
+        }
+        map.put(key,result);
+        return result;
+    }
 }
