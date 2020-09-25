f2730f7979d174717f2bd2f0941eb4bc68dc985f
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 21:36:31 +0530

    Update 050_PowXN50.java

--- 050_PowXN50.java
+++ 050_PowXN50.java
@@ -1,68 +1,103 @@
-/**
- * Implement pow(x, n), which calculates x raised to the power n (xn).
- * 
- * Example 1:
- * Input: 2.00000, 10
- * Output: 1024.00000
- * 
- * Example 2:
- * Input: 2.10000, 3
- * Output: 9.26100
- * 
- * Example 3:
- * Input: 2.00000, -2
- * Output: 0.25000
- * Explanation: 2-2 = 1/22 = 1/4 = 0.25
- * 
- * Note:
- * -100.0 < x < 100.0
- * n is a 32-bit signed integer, within the range [−231, 231 − 1]
- */
-
-public class PowXN50 {
-    /**
-     * https://leetcode.com/problems/powx-n/solution/
-     */
+class Solution {
     public double myPow(double x, int n) {
-        long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        return fastPow(x, N);
-    }
-
-    private double fastPow(double x, long n) {
-        if (n == 0) {
+        /*
+            fast exponentiation method
+            2 Cases:
+                n odd
+                n even
+        
+        */
+        if(n==0){
             return 1.0;
         }
-        double half = fastPow(x, n / 2);
+        else if(n==1){
+            return x;
+        }
+        else if(n==-1){
+            return (1.0/x);
+        }
+        if(n%2==0){
+            return myPow(x*x, n/2);
+        }
+        else{
+            if(n<0){
+                double ans = myPow(x*x, (n+1)/2);
+                return (1.0/x)*ans;
+            }
+            else{
+                double ans = myPow(x*x, (n-1)/2);
+                return x*ans;
+            }
+        }
+    }
+}
+**************************************************************************************************
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0 || x == 1)
+            return 1;
+        
+        if (x == -1 && n % 2 == 0)
+            return 1.0;
+        else if (x == -1 && n % 2 == 1)
+            return -1.0;
+        
+        if (n > 5000000 && x > 1)
+            return Integer.MAX_VALUE;
+        else if (n > 5000000 && x < 1 && x > 0)
+            return 0.0;
+        
+        if (n < -5000000)
+            return 0.0;
+        
+        if (n == -1.00000)
+            return 1 / x;
+        
+        double ans = 1.0;
+            
+        for (int i = 0; i < Math.abs(n); i++)
+            ans *= x;
+        
+        return n > 0 ? ans : 1 / ans;
+    }
+}
+
+************************************************************
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+        if (n == 0) return 1.0;
+        
+        double half = myPow(x, n/2);
         if (n % 2 == 0) {
             return half * half;
+        } else if (n < 0) {
+            return (1.0 / x) * half * half;
         } else {
-            return half * half * x;
+            return x * half * half;
         }
     }
-
-
-    /**
-     * https://leetcode.com/problems/powx-n/solution/
-     */
-    public double myPow2(double x, int n) {
-        long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        double ans = 1;
-        double current_product = x;
-        for (long i = N; i > 0; i /= 2) {
-            if ((i % 2) == 1) {
-                ans = ans * current_product;
-            }
-            current_product = current_product * current_product;
-        }
-        return ans;
-    }
-
 }
+
+***********************************************************************
+class Solution {
+    public double myPow(double x, int n) {
+        
+        if(n == 0)
+            return 1;
+        
+        double temp = myPow(x,n/2);
+        
+        if((n%2 == 0)){
+            return temp*temp;
+        }
+        else{
+            if(n > 0)
+                return x*temp*temp;
+            else
+                return (temp*temp)/x;
+        }        
+    }
+}
+
+****************************************************************
