ec3317030262a26da8bfd683eecdfc8ec45ce980
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 09:47:04 +0530

    Update 007_Reverse_Integer.java

--- 007_Reverse_Integer.java
+++ 007_Reverse_Integer.java
@@ -11,3 +11,60 @@ class Solution {
         return (int) res;
     }
 }
+
+******************************************************************************
+
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+        int reminder = x;
+
+        while (reminder != 0) {
+            int lastDigit = reminder % 10;
+            int nextRes = result * 10 + lastDigit;
+            
+            /*
+                expression '(nextRes - lastDigit) / 10' should return us the current result
+                if it's different - it means that we have an int overflow 
+            */
+            if ((nextRes - lastDigit) / 10 != result) {
+                return 0;
+            }
+
+            result = nextRes;
+            reminder /= 10;
+        }
+        
+        return result;
+    }
+}
+
+************************************************************************
+class Solution {
+    public int reverse(int x) {
+        
+        boolean isNegative = x<0;
+        if(isNegative){
+            x=x*-1;
+        }
+        
+        long newNumber = 0;
+        
+        while(x>0){
+            int lastDigitOfX = x%10;
+            newNumber = (newNumber*10) + lastDigitOfX;
+            x = x/10;
+        }
+        
+        if(newNumber > Integer.MAX_VALUE){
+            return 0;
+        }
+        
+        if(isNegative){
+            newNumber = newNumber*-1;
+        }
+        
+        return (int)newNumber;
+        
+    }
+}
