bc4f947bc456dd5549eb364dd95acee6d15efd0a
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 21:13:14 +0530

    Update 044_WildcardMatching44.java

--- 044_WildcardMatching44.java
+++ 044_WildcardMatching44.java
@@ -1,182 +1,112 @@
-/**
- * Implement wildcard pattern matching with support for '?' and '*'.
- *
- * '?' Matches any single character.
- * '*' Matches any sequence of characters (including the empty sequence).
- *
- * The matching should cover the entire input string (not partial).
- *
- * The function prototype should be:
- * bool isMatch(const char *s, const char *p)
- *
- * Some examples:
- * isMatch("aa","a") → false
- * isMatch("aa","aa") → true
- * isMatch("aaa","aa") → false
- * isMatch("aa", "*") → true
- * isMatch("aa", "a*") → true
- * isMatch("ab", "?*") → true
- * isMatch("aab", "c*a*b") → false
- *
- */
+class Solution {
+    public boolean isMatch(String s, String p) { 
+        int m = s.length(), n = p.length();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (p.charAt(i) == '*') count++;
+        }
+        if (count==0 && m != n) return false;
+        else if (n - count > m) return false;
+        
+        while(p.contains("**")) p = p.replace("**","*");
+        
+        HashMap<String, HashMap<String,Boolean>> cache = new HashMap<>();        
+        return isMatchHelper(s,p, cache);
+        
+    }
+    
+    public boolean isMatchHelper(String s, String p, HashMap<String, HashMap<String,Boolean>> cache){
+        if(cache.containsKey(s)){
+            HashMap<String, Boolean> incache = cache.get(s);
+            if(incache.containsKey(p)) return incache.get(p);
+        }
+        if(s.length()==0){
+            p = p.replace("*","");
+        }
+        if(s.length()==0 && p.length()==0) return true;
+        if(s.length()==0 || p.length()==0) return false;
+        if(s.charAt(0)!=p.charAt(0) && (p.charAt(0)!= '?' && p.charAt(0)!='*')) return false;
+        
+        boolean isMatch = false;
+            if(p.charAt(0)==s.charAt(0) || p.charAt(0)=='?'){
+                isMatch = isMatch || isMatchHelper(s.substring(0+1),p.substring(0+1),cache);
+            }
+            if(p.charAt(0)=='*'){
+                isMatch = isMatch || isMatchHelper(s,"" + p.substring(0+1), cache);
+            }        
+            if(p.charAt(0)=='*'){
+                isMatch = isMatch || isMatchHelper(s.substring(0+1),"*" + p.substring(0+1), cache);
+            }
+        
+        if(cache.containsKey(s)){
+            HashMap<String, Boolean> incache = cache.get(s);
+            incache.put(p,isMatch);
+        }else{
+            HashMap<String, Boolean> incache = new HashMap<>();
+            incache.put(p,isMatch);
+            cache.put(s,incache);
+        }
+        
+        return isMatch;        
+    }
+}
 
-public class WildcardMatching44 {
-    // time limit exceeded
+*****************************************************************************
+
+class Solution {
+	
+	public boolean isMatch (String s, String p) {
+		
+		boolean[][] dp = new boolean[s.length () + 1][p.length () + 1];
+		dp[0][0] = true;
+		
+		for (int i = 0; i < p.length () && p.charAt (i++) == '*';) {
+			dp[0][i] = true;
+		}
+		
+		for (int i = 1; i <= s.length (); i++) {
+			for (int j = 1; j <= p.length (); j++) {
+				if (s.charAt (i - 1) == p.charAt (j - 1) || p.charAt (j - 1) == '?') {
+					dp[i][j] = dp[i - 1][j - 1];
+				}
+				else if (p.charAt (j - 1) == '*') {
+					dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+				}
+			}
+		}
+		
+		return dp[s.length ()][p.length ()];
+	}
+}
+
+
+************************************************************************************
+class Solution {
     public boolean isMatch(String s, String p) {
-        if (s == null || p == null) return false;
-        return isMatch(s, 0, p, 0);
-    }
-
-    private boolean isMatch(String s, int i, String p, int j) {
-        if (i >= s.length()) return restAreStars(p, j);
-        if (j >= p.length()) return false;
-
-        switch (p.charAt(j)) {
-            case '?':
-                return isMatch(s, i+1, p, j+1);
-            case '*':
-                while (j+1 < p.length() && p.charAt(j+1) == '*') j++;
-                return isMatch(s, i, p, j+1) || isMatch(s, i+1, p, j+1) || isMatch(s, i+1, p, j);
-            default:
-                return s.charAt(i) == p.charAt(j) && isMatch(s, i+1, p, j+1);
-        }
-    }
-
-    private boolean restAreStars(String p, int j) {
-        while (j < p.length()) {
-            if (p.charAt(j) != '*') return false;
-            j++;
-        }
-        return true;
-    }
-
-
-    // DP, 2D space
-    public boolean isMatch2(String s, String p) {
-        if (s == null || p == null) return false;
-
-        boolean[][] dp = new boolean[p.length()+1][s.length()+1];
-        dp[0][0] = true;
-        for (int j=1; j<=s.length(); j++) dp[0][j] = false;
-        for (int i=1; i<=p.length(); i++) dp[i][0] = p.charAt(i-1) == '*' ? dp[i-1][0] : false;
-
-        for (int i=1; i<=p.length(); i++) {
-            char c = p.charAt(i-1);
-            for (int j=1; j<=s.length(); j++) {
-                switch (c) {
-                    case '?':
-                        dp[i][j] = dp[i-1][j-1];
-                        break;
-                    case '*':
-                        dp[i][j] = dp[i-1][j] || dp[i-1][j-1] || dp[i][j-1];
-                        break;
-                    default:
-                        dp[i][j] = c == s.charAt(j-1) && dp[i-1][j-1];
+        int ls = s.length();
+        int lp = p.length();
+        boolean[][] d = new boolean[ls + 1][lp + 1];
+        int pIdx;
+        int sIdx;
+        
+        for(int i = 0; i <= ls; i++){
+            for(int j = 0; j <= lp; j++){
+                sIdx = i - 1;
+                pIdx = j - 1;
+                if( i == 0 && j == 0){
+                    d[i][j] = true;
+                } else if( i == 0){
+                    d[i][j] = (p.charAt(pIdx) == '*') && d[i][j - 1];
+                } else if( j == 0){
+                    d[i][j] = false;
+                } else if(p.charAt(pIdx) == '*'){
+                        d[i][j] = d[i][j - 1] | d[i - 1][j];
+                } else if(p.charAt(pIdx) == '?' || s.charAt(sIdx) == p.charAt(pIdx)){
+                        d[i][j] = d[i - 1][j - 1];
                 }
             }
         }
-
-        return dp[p.length()][s.length()];
+        
+        return d[ls][lp];
     }
-
-
-    // DP, 1D space
-    public boolean isMatch3(String s, String p) {
-        if (s == null || p == null) return false;
-
-        boolean[] dp = new boolean[s.length()+1];
-        dp[0] = true;
-        for (int j=1; j<=s.length(); j++) dp[j] = false;
-
-        for (int i=1; i<=p.length(); i++) {
-            boolean old = dp[0];
-            boolean pre = dp[0];
-            dp[0] = p.charAt(i-1) == '*' ? dp[0] : false;
-            char c = p.charAt(i-1);
-            for (int j=1; j<=s.length(); j++) {
-                old = dp[j];
-                switch (c) {
-                    case '?':
-                        dp[j] = pre;
-                        break;
-                    case '*':
-                        dp[j] = old || pre || dp[j-1];
-                        break;
-                    default:
-                        dp[j] = c == s.charAt(j-1) && pre;
-                }
-                pre = old;
-            }
-        }
-
-        return dp[s.length()];
-    }
-
-
-    /**
-     * https://leetcode.com/problems/wildcard-matching/discuss/17810/Linear-runtime-and-constant-space-solution
-     */
-    boolean isMatch4(String str, String pattern) {
-        int s = 0, p = 0, match = 0, starIdx = -1;
-        while (s < str.length()){
-            // advancing both pointers
-            if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
-                s++;
-                p++;
-            }
-            // * found, only advancing pattern pointer
-            else if (p < pattern.length() && pattern.charAt(p) == '*'){
-                starIdx = p;
-                match = s;
-                p++;
-            }
-            // last pattern pointer was *, advancing string pointer
-            else if (starIdx != -1){
-                p = starIdx + 1;
-                match++;
-                s = match;
-            }
-            //current pattern pointer is not star, last patter pointer was not *
-            //characters do not match
-            else return false;
-        }
-
-        //check for remaining characters in pattern
-        while (p < pattern.length() && pattern.charAt(p) == '*') p++;
-
-        return p == pattern.length();
-    }
-
-
-    public boolean isMatch5(String s, String p) {
-        if (p.length() == 0) return s.length() == 0;
-        Map<Integer, Boolean> memo = new HashMap<>();
-        return isMatch(s.toCharArray(), 0, p.toCharArray(), 0, memo);
-    }
-
-    public boolean isMatch(char[] s, int i, char[] p, int j, Map<Integer, Boolean> memo) {
-        int idx = i * p.length + j;
-        if (memo.containsKey(idx)) return memo.get(idx);
-        if (j == p.length) return i == s.length;
-
-        boolean res = false;
-        char pChar = p[j];
-        if (i == s.length) {
-            res = pChar == '*' && isMatch(s, i, p, j+1, memo);
-        } else {
-            char sChar = s[i];
-            if (pChar == '?') {
-                res = isMatch(s, i+1, p, j+1, memo);
-            } else if (pChar == '*') {
-                res = isMatch(s, i+1, p, j+1, memo) ||
-                        isMatch(s, i, p, j+1, memo) ||
-                        isMatch(s, i+1, p, j, memo);
-            } else {
-                res = pChar == sChar && isMatch(s, i+1, p, j+1, memo);
-            }
-        }
-        memo.put(idx, res);
-        return res;
-    }
-
 }
