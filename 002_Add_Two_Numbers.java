aee6dd984ef2cd61ee8de4f7b5d71a3eaf7d27fb
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 09:06:15 +0530

    Update 002_Add_Two_Numbers.java

--- 002_Add_Two_Numbers.java
+++ 002_Add_Two_Numbers.java
@@ -1,30 +1,62 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-public class Solution {
-    // example in leetcode book
+class Solution {
+ 
+    ListNode th =null;
+    ListNode tt = null;
+    
+    public void addLast(int x){
+        ListNode nn = new ListNode(x);
+        if(th == null){
+            th = nn;
+            tt = nn;
+        }else{
+            tt.next = nn;
+            tt = nn;
+        }   
+    }
+    
+    
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    	ListNode dummyHead = new ListNode(0);
-    	ListNode p = l1, q= l2, curr = dummyHead;
-    	int carry = 0;
-    	while (p != null || q!= null) {
-    		int x = (p != null) ? p.val : 0;
-    		int y = (q != null) ? q.val : 0;
-    		int digit = carry + x + y;
-    		carry = digit / 10;
-    		curr.next = new ListNode(digit % 10);
-    		curr = curr.next;
-    		if (p != null) p = p.next;
-    		if (q != null) q = q.next;
-    	}
-    	if (carry > 0) {
-    		curr.next = new ListNode(carry);
-    	}
-    	return dummyHead.next;
+        if(l1 == null  && l2 ==  null)return null;
+        if(l1==null) return l2;
+        if(l2 == null) return l1;
+        
+       ListNode c1 = (l1);
+         ListNode c2 = (l2);
+        int carry = 0;
+        int add1 = 0;
+        while(c1!=null && c2!=null){
+            int x = c1.val + c2.val + carry;
+             carry = x/10; 
+            add1 = x%10;
+            addLast(add1);
+            c1= c1.next;
+            c2 = c2.next;
+        }
+        
+        if(carry == 0){
+            tt.next = (c1 == null?c2:c1);
+            return th;
+        }
+        
+        
+        while(c1!=null){
+            int x = c1.val + carry;
+            carry = x/10;
+            add1 = x%10;
+            addLast(add1);
+            c1 = c1.next;
+        }
+        
+        while(c2!=null){
+            int x = c2.val + carry;
+            carry = x/10;
+            add1 = x%10;
+            addLast(add1);
+            c2 = c2.next;
+        }
+        
+        if(carry!=0) addLast(carry);
+        
+      return th;   
     }
 }
