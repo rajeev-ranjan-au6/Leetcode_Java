f5e7bbb4c13fa625d16acacdeb2eb953d827eba1
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 10:42:35 +0530

    Update 017_LetterCombinationsOfAPhoneNumber17.java

--- 017_LetterCombinationsOfAPhoneNumber17.java
+++ 017_LetterCombinationsOfAPhoneNumber17.java
@@ -1,97 +1,104 @@
-/**
- * Given a digit string, return all possible letter combinations that the number
- * could represent.
- *
- * A mapping of digit to letters (just like on the telephone buttons) is given below.
- *
- * Image: http://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Telephone-keypad2.svg/200px-Telephone-keypad2.svg.png
- *
- * Input:Digit string "23"
- * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
- *
- *
- * Note:
- * Although the above answer is in lexicographical order, your answer could be
- * in any order you want.
- *
- */
-
-
-public class LetterCombinationsOfAPhoneNumber17 {
-    /**
-     * https://discuss.leetcode.com/topic/8465/my-java-solution-with-fifo-queue
-     */
+class Solution {
+     private List<String> bfs(int[] numbers, int n, String[] table) {
+         if (n == 0) {
+            return Collections.emptyList();
+         }
+         List<String> result = new ArrayList<>();
+         Queue<String> queue = new ArrayDeque<>();
+         queue.offer("");
+         while (!queue.isEmpty()) {
+             String current = queue.poll();
+             if (current.length() == n) {
+                 result.add(current);
+             }
+             else {
+                 for (char letter : table[numbers[current.length()]].toCharArray()) {
+                     queue.offer(current + letter);
+                 }
+             }
+         }
+         return result;
+    }
+    
     public List<String> letterCombinations(String digits) {
-        LinkedList<String> ans = new LinkedList<String>();
-        int L = digits.length();
-        if (L == 0) return ans;
+        String[] table = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        int n = digits.length();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i ++) {
+            char k = digits.charAt(i);
+            numbers[i] = Character.getNumericValue(k);
+        }
+        return bfs(numbers, n, table);
+    }
+}
 
-        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        ans.add("");
-        for(int i =0; i<digits.length();i++){
-            int x = Character.getNumericValue(digits.charAt(i));
-            while(ans.peek().length()==i){
-                String t = ans.remove();
-                for(char s : mapping[x].toCharArray())
-                    ans.add(t+s);
+****************************************************************************************************************
+
+class Solution {
+    public static final String[] codes = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits.length() == 0) {
+            return result;
+        }
+        
+        char[] partial = new char[digits.length()];
+        int start = 0;
+        
+        permHelper(digits, start, partial, result);
+        
+        return result;
+    }
+    
+    public void permHelper(String input, int idx, char[] partial, List<String> result) {
+        if (idx == input.length()) {
+            result.add(new String(partial));
+        } else {
+            for (int i = 0; i < codes[input.charAt(idx) - '0'].length(); i++) {
+                String in = codes[input.charAt(idx) - '0'];
+                partial[idx] = in.charAt(i);
+                permHelper(input, idx + 1, partial, result);
             }
         }
-        return ans;
+    }    
+}
+
+***********************************************************************************************************
+
+class Solution {
+    private static char[][] phone; 
+    
+    static {
+        phone = new char[8][];
+        phone['2' - '2'] = "abc".toCharArray();
+        phone['3' - '2'] = "def".toCharArray();
+        phone['4' - '2'] = "ghi".toCharArray();
+        phone['5' - '2'] = "jkl".toCharArray();
+        phone['6' - '2'] = "mno".toCharArray();
+        phone['7' - '2'] = "pqrs".toCharArray();
+        phone['8' - '2'] = "tuv".toCharArray();
+        phone['9' - '2'] = "wxyz".toCharArray(); 
     }
-
-
-    /**
-     * https://discuss.leetcode.com/topic/6380/my-recursive-solution-using-java
-     */
-    private static final String[] KEYS = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-
-    public List<String> letterCombinations2(String digits) {
-      List<String> ret = new LinkedList<String>();
-          if (digits.length() == 0) return ret;
-
-      combination("", digits, 0, ret);
-      return ret;
-    }
-
-    private void combination(String prefix, String digits, int offset, List<String> ret) {
-      if (offset >= digits.length()) {
-        ret.add(prefix);
-        return;
-      }
-      String letters = KEYS[(digits.charAt(offset) - '0')];
-      for (int i = 0; i < letters.length(); i++) {
-        combination(prefix + letters.charAt(i), digits, offset + 1, ret);
-      }
-    }
-
-
-
-    private String[] alphas = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-    public List<String> letterCombinations4(String digits) {
-        List<String> res = new ArrayList<>();
-        if (digits == null || digits.length() == 0) return res;
-
-        String[] letters = new String[digits.length()];
-        for (int i=0; i<digits.length(); i++) letters[i] = alphas[Character.getNumericValue(digits.charAt(i))];
-
-        helper(res, letters, 0, new StringBuilder());
-
-        return res;
-    }
-
-    private void helper(List<String> res, String[] letters, int i, StringBuilder sb) {
-        if (i >= letters.length) {
-            res.add(sb.toString());
-            return;
+    
+    public List<String> letterCombinations(String digits) {
+        List<String> output = new ArrayList<>();
+        char[] input = digits.toCharArray();
+        if (input.length == 0) {
+            return output;
         }
-
-        char[] chars = letters[i].toCharArray();
-        for (char c: chars) {
-            sb.append(c);
-            helper(res, letters, i+1, sb);
-            sb.deleteCharAt(sb.length()-1);
+        backtrack(input, 0, new char[input.length], output);
+        return output;
+    }
+    
+    private void backtrack(char[] input, int pos, char[] path, List<String> output) {
+        if (pos == input.length) {
+            output.add(new String(path));
+        } else {
+            char[] phoneDigs = phone[input[pos] - '2'];
+            for (char dig: phoneDigs) {
+                path[pos] = dig;
+                backtrack(input, pos + 1, path, output);
+            }
         }
     }
-
 }
