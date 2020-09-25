2cbf386e9fd44ef30fd7277a229984cb6e7447a7
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-25 12:01:59 +0530

    Update 023_MergeKSortedLists23.java

--- 023_MergeKSortedLists23.java
+++ 023_MergeKSortedLists23.java
@@ -1,200 +1,124 @@
-/**
- * Merge k sorted linked lists and return it as one sorted list.
- */
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-
-
-public class MergeKSortedLists23 {
-    /**
-     * https://discuss.leetcode.com/topic/26095/my-simple-java-solution-use-recursion
-     */
-    public static ListNode mergeKLists(ListNode[] lists){
-        return partion(lists,0,lists.length-1);
-    }
-
-    public static ListNode partion(ListNode[] lists,int s,int e){
-        if(s==e)  return lists[s];
-        if(s<e){
-            int q=(s+e)/2;
-            ListNode l1=partion(lists,s,q);
-            ListNode l2=partion(lists,q+1,e);
-            return merge(l1,l2);
-        }else
-            return null;
-    }
-
-    //This function is from Merge Two Sorted Lists.
-    public static ListNode merge(ListNode l1,ListNode l2){
-        if(l1==null) return l2;
-        if(l2==null) return l1;
-        if(l1.val<l2.val){
-            l1.next=merge(l1.next,l2);
-            return l1;
-        }else{
-            l2.next=merge(l1,l2.next);
-            return l2;
-        }
-    }
-
-
-    /**
-     * https://discuss.leetcode.com/topic/2780/a-java-solution-based-on-priority-queue
-     */
-    public ListNode mergeKLists2(ListNode[] lists) {
-        if (lists==null||lists.length==0) return null;
-
-        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length,new Comparator<ListNode>(){
-            @Override
-            public int compare(ListNode o1,ListNode o2){
-                if (o1.val<o2.val)
-                    return -1;
-                else if (o1.val==o2.val)
-                    return 0;
-                else
-                    return 1;
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(ListNode list: lists) {
+            while(list != null) {
+                pq.add(list.val);
+                list = list.next;
             }
-        });
-
-        ListNode dummy = new ListNode(0);
-        ListNode tail=dummy;
-
-        for (ListNode node:lists)
-            if (node!=null)
-                queue.add(node);
-
-        while (!queue.isEmpty()){
-            tail.next=queue.poll();
-            tail=tail.next;
-
-            if (tail.next!=null)
-                queue.add(tail.next);
         }
-        return dummy.next;
-    }
-
-
-    /**
-     * https://discuss.leetcode.com/topic/26095/my-simple-java-solution-use-recursion
-     */
-    public ListNode mergeKLists3(ListNode[] lists) {
-        return mL(lists, 0, lists.length - 1);
-    }
-
-    private ListNode mL(ListNode[] lists, int l, int r) {
-        if (r < l) return null;
-        if (r == l) return lists[r];
-
-        int mid = (l + r) / 2;
-        ListNode a = mL(lists, l, mid), b = mL(lists, mid + 1, r);
-        ListNode dmHead = new ListNode(0), cur = dmHead;
-        while (a != null && b != null) {
-            if (a.val < b.val) {
-                cur.next = a;
-                a = a.next;
-            } else {
-                cur.next = b;
-                b = b.next;
-            }
-            cur = cur.next;
+        
+        ListNode curr = new ListNode(0);
+        ListNode head = curr;
+        while(!pq.isEmpty()) {
+            ListNode t = new ListNode(pq.poll());
+            curr.next = t;
+            curr = curr.next;
         }
-        cur.next = (a != null) ? a : b;
-
-        return dmHead.next;
+        return head.next;
     }
-
-
-    public ListNode mergeKLists4(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
-        ListNode dummy = new ListNode(0);
-        ListNode p = dummy;
-        PriorityQueue<ListNode> q = new PriorityQueue<ListNode>((l1, l2) -> Integer.compare(l1.val, l2.val));
-        for (ListNode node: lists) {
-            if (node != null) q.add(node);
-        }
-        while (!q.isEmpty()) {
-            ListNode curr = q.poll();
-            if (curr.next != null) q.add(curr.next);
-            curr.next = null;
-            p.next = curr;
-            p = p.next;
-        }
-
-        return dummy.next;
-    }
-
-
-    public ListNode mergeKLists5(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
-
-        int len = lists.length;
-        int interval = 1;
-        while (interval < len) {
-            for (int i=0; i < len-interval; i+=interval*2)
-                lists[i] = mergeTwoLists5(lists[i], lists[i + interval]);
-            interval *= 2;
-        }
-
-        return lists[0];
-    }
-
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode p = dummy;
-        while (l1 != null && l2 != null) {
-            if (l1.val >= l2.val) {
-                p.next = l2;
-                l2 = l2.next;
-            } else {
-                p.next = l1;
-                l1 = l1.next;
-            }
-            p = p.next;
-            p.next = null;
-        }
-        if (l1 != null) p.next = l1;
-        if (l2 != null) p.next = l2;
-        return dummy.next;
-    }
-
-
-    public ListNode mergeKLists6(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
-        return mergeKLists(lists, 0, lists.length-1);
-    }
-
-
-    public ListNode mergeKLists(ListNode[] lists, int i, int j) {
-        if (i == j) return lists[i];
-        else if (j - i == 1) return mergeTwoLists(lists[i], lists[j]);
-        else {
-            int mid = (i + j) / 2;
-            return mergeTwoLists(mergeKLists(lists, i, mid), mergeKLists(lists, mid+1, j));
-        }
-    }
-
-    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode prehead = new ListNode(-1);
-        ListNode prev = prehead;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                prev.next = l1;
-                l1 = l1.next;
-            } else {
-                prev.next = l2;
-                l2 = l2.next;
-            }
-            prev = prev.next;
-        }
-        prev.next = l1 == null ? l2 : l1;
-        return prehead.next;
-    }
-
 }
+
+****************************************************************************
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        if(lists==null || lists.length==0)
+            return null;
+        
+        List<Integer>list=new ArrayList<Integer>();
+        for(int i=0;i<lists.length;i++){
+            ListNode cur=lists[i];
+            while(cur!=null){
+                list.add(cur.val);
+                cur=cur.next;
+            }
+        }
+        ListNode head=null;
+        if(list.size()>0){
+            Collections.sort(list);
+            head=new ListNode(list.get(0));
+            ListNode cur=head;
+            for(int i=1;i<list.size();i++){
+             ListNode node=new ListNode(list.get(i));
+             cur.next=node;
+             cur=cur.next;
+          }
+        }
+         
+        return head;
+    }
+}
+
+**************************************************************************
+
+class Solution {
+	public ListNode mergeKLists(ListNode[] lists) {
+
+		//using lambda define a PriorityQueue to sort the heads 
+		Queue<ListNode> queue = new PriorityQueue<>((l1, l2) -> {return l1.val - l2.val;});   
+		for(ListNode list : lists) if(list != null) queue.offer(list);
+		if(queue.size() == 0) return null;
+
+		//keep polling the head with minimal val and put the next node of that head into PriorityQueue
+		ListNode head = queue.poll();
+		ListNode pointer = head;
+		if(head.next != null) queue.offer(head.next);
+		while(!queue.isEmpty()) {
+			pointer.next = queue.poll();
+			pointer = pointer.next;
+			if(pointer.next != null) queue.offer(pointer.next);
+		}
+
+		return head;
+	}
+}
+
+******************************************************************
+
+public class Solution {
+
+	// using priority queue
+	public ListNode mergeKLists(ListNode[] lists) 
+	{
+		int n = lists.length;
+		if(n == 0) return null;
+		if(n == 1) return lists[0];
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+
+		// add all items form all lists
+		for(int i=0;i<lists.length;i++)
+		{
+			ListNode c = lists[i];
+			while(c != null)
+			{
+				q.add(c.val);
+				c = c.next;
+			}
+		}
+		ListNode res = null;
+		ListNode c = null;
+		ListNode last = null;
+
+		// convert q back to listNode
+		while(q.isEmpty() == false)
+		{
+			c = new ListNode(q.poll());
+			if(last != null)
+			{
+				last.next = c;
+			}
+			last = c;
+			if(res == null)
+			{
+				res = last;
+			}
+		}
+
+		return res;
+	}
+}
+
+************************************************************************
+
